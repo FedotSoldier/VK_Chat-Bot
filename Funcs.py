037db86b50a6aae4c053f08015516b2a3a791371
fedotsoldier <mumikrol@gmail.com>
2018-11-19 20:53:39 +0300

    [ 9 ] В игре в города добавлены функция подсказки и списка букв, на которые закончились города
        Теперь бот может подсказать город и прислать список букв, на которые закончились города.
        Также, теперь бот в слове(присланном  городе) ищет первую букву с конца, на которую есть города в базе(csv таблице) и называет города на нее, оповещая при этом пользователя, почему он назвал город не на последнюю букву его города. Аналогично бот говорит на какую букву его города надо называть город пользователю, если город бота оканчивается на ту букву, на которую нет городов в базе.

--- Funcs.py
+++ Funcs.py
@@ -172,6 +172,11 @@ class CityPlayer:
 		self.used_cities = pd.Series(['Something'])
 		# Показатель выигрыша компьютера
 		self.comp_won = None
+		# Список с буквами, на которые больше не осталось городов
+		self.lost_letters = []
+		# Список с буквами, на которые больше не осталось городов,
+		# о которых еще не оповещен пользователь
+		self.letters_to_notify = []
 
 	def next_city(self, cur):
 		# Ответ компьютера игроку
@@ -180,6 +185,25 @@ class CityPlayer:
 		if cur == 'стоп':
 			self.comp_won = True
 			answer = 'Игра окончена. Спасибо за игру!'
+		# Может игрок хочет получить подсказку:
+		elif cur == 'подсказка':
+			if self.last_word == None:
+				self.cities.index = range(0, len(self.cities))
+				city = self.cities[random.randint(0, len(self.cities) - 1)]
+				answer = '''Ваш ход первый. Вы можете назвать
+						  любой город, например: "{}" '''.format(city)
+			else:
+				cities = self.table[self.cities.str.startswith(self.last_word[-1])]['city']
+				cities.index = range(0, len(cities))
+				answer = cities[random.randint(0, len(cities) - 1)]
+				answer = self.table[self.cities == answer]['city_to_output']
+		# Пользователь хочет получить список букв, на которые нет городов:
+		elif cur == '/nonused':
+			if len(self.lost_letters) == 0:
+				answer = 'Буквы, на которые нет городов, пока не найдены'
+			else:
+				answer = ''' Список букв, на которые
+							 больше нет городов:\n{}'''.format('\n'.join(self.lost_letters))
 		# Проверяем, не первый ли это ход(игрока)
 		elif self.last_word is not None:
 			# Если игрок назвал слово на последнюю
@@ -189,8 +213,7 @@ class CityPlayer:
 
 			# Если игрок назвал город не на последнюю букву предыдущего города:
 			elif cur[0] != self.last_word[-1]:
-				answer = '''Вы должны назвать новый город на последнюю букву предыдущего.
-							Последний названный город: {}'''.format(self.last_word)
+				answer = 'Вы должны назвать новый город на букву "{}"'.format(self.last_word[-1])
 
 		# Если это первый ход(первым ходит игрок):
 		elif self.last_word is None:
@@ -199,51 +222,98 @@ class CityPlayer:
 		return answer
 
 	def get_city(self, cur):
+		# Показатель того, что кроме города-ответа
+		# в ответе присутствуют еще какие-либо объяснения и тп
+		is_addition = False
+		# Ответ пока пустой
+		answer = ''
 		# Если город есть в базе данных:
 		if len(self.cities[self.cities == cur]) > 0:
 			# Удаляем слово из базы данных
 			self.remove_city(cur)
-			# Выбираем из бд все слова, которые начинаются
-			#  на последнюю букву слова, отправленного игроком
-			the_right_words = self.table[self.cities.str.startswith(cur[-1])]['city']
+			# Сохраняем слово до обрезания
+			cur_before = cur
+			# Обрезаем слово, пока на его последнюю букву
+			# не будут иметься города в таблице
+			cur = self.prepare_city(cur)
 			# Если остались слова на нужную букву:
-			if len(the_right_words) > 0:
+			if cur != '':
+				# Если слово было обрезано:
+				if cur != cur_before:
+					# Добавляем в ответ объяснение того, почему отправляем
+					# слово не на последнюю букву слова пользователя
+					answer += ''' Первая с конца буква вашего города, 
+								  на которую остались города - {} '''.format(cur[-1])
+					is_addition = True
+				# Выбираем из бд все слова, которые начинаются
+				# на последнюю букву слова, отправленного игроком
+				the_right_words = self.table[self.cities.str.startswith(cur[-1])]['city']
 				# Определяем новые индексы от нуля до конца, а то останутся
 				# те же, которые были у этих городов в общей таблицы
 				the_right_words.index = range(0, len(the_right_words))
 				# Случайное слово, которое будем отправлять игроку
 				cur_word = the_right_words[random.randint(0, len(the_right_words) - 1)]
 				# Записываем этот случайный город(cur_word) в ответ
-				# Пока что это город с маленьким регистром, с замененными на ё е и тд.
-				answer = cur_word
+				# Пока что это город с маленьким регистром, с е замененными на ё и тд.
+				city = cur_word
 				# Выбираем соседнюю ячейку таблицы для этого города -
 				# то же название, записанное более приятно для глаз(по правилам)
-				answer = self.table[self.cities == answer]['city_to_output']
-				answer.index = range(len(answer))
-				answer = answer[0]
+				city = self.table[self.cities == city]['city_to_output']
+				# Настраиваем индексы для списка этих городов(city)
+				city.index = range(len(city))
+				# В конце добавим первый элемент из city
+				# (а там всего один элемент) в ответ
+				# answer += '\n' + city[0]
+				# Сохраняем наш ответ до обрезания :)
+				cur_before = cur_word
 				# Удаляем город из базы
 				self.remove_city(cur_word)
+				# Доводим до нужной кондиции cur_word - может
+				# не быть городов на его последнюю букву
+				cur_word = self.prepare_city(cur_word)
+				# Если появились новые буквы, на которые больше нет городов:
+				if len(self.letters_to_notify) > 0:
+					# Оповещаем об этом пользователя
+					answer += '''\nБольше нет городов на букву(ы): {}'''.format(', '.join(self.letters_to_notify))
+					self.letters_to_notify = []
+					is_addition = True
+				if cur_word != cur_before:
+					if cur_word != '':
+						answer += '''\nПервая с конца буква моего города("{}"), на которую остались города -
+									   это "{}". Назовите город на неё. '''.format(city[0], cur_word[-1])
+					# Если нет городов ни на одну из букв нашего города:
+					elif cur_word == '':
+						answer += '''\nК сожалению не осталось городов, ни на одну из букв
+									   города "{}". К сожалению вы проиграли'''.format(city[0])
+						self.comp_won = True
+					is_addition = True
+				# Добавляем в ответ город, выбранный компьютером(делаем это только сейчас,
+				# чтобы ответ был в конце - так удобнее - не надо искать его где-то в середине)
+				answer += '\n'
+				# Если были какие-либо объяснения в ответе(информация кроме города-ответа):
+				if is_addition:
+					# То помимо перед городом-ответом вставляем информацию,
+					# что именно это мой ответ - чтобы было сразу видно,
+					# где объяснение, а где ответ
+					answer += 'Мой город: '
+				answer += city[0]
 				# Последнее названное(компьютером) слово - это текущее слово
 				self.last_word = cur_word
-				# Если на последнюю букву отправляемого слова нет городов:
-				if len(self.table[self.cities.str.startswith(cur_word[-1])]['city']) == 0:
-					answer += '''\nУвы, больше не осталось городов, начинающихся на букву: "{}".
-								   К сожалению вы проиграли. :('''.format(cur_word[-1])
-					self.comp_won = True
 
 			else:
-				answer = 'Поздравляем. Слов на букву "{}" больше нет. Вы выиграли!!!'.format(cur[-1])
+				answer = ''' Поздравляем. Больше не осталось городов, ни на одну
+							 из букв вашего города. Вы выиграли!!! '''.format(cur[-1])
 				self.comp_won = False
 
 		# Если город есть в базе данных использованных городов:
 		elif int(len(self.used_cities[self.used_cities == cur])) > 0:
 			answer = '''Такой город уже был. Попробуйте еще.
-						Предыдущий город: {}'''.format(self.last_word)
+						Назовите город на букву "{}" '''.format(self.last_word[-1])
 
 		# Если же города, названного игроком, нет ни в одной в базе:
 		else:
 			answer = '''Такого города нет в базе. Попробуйте какой-нибудь другой.
-						Последний город: {}'''.format(self.last_word)
+						Назовите город на букву "{}" '''.format(self.last_word[-1])
 		return answer
 
 	def remove_city(self, word):
@@ -251,6 +321,18 @@ class CityPlayer:
 		self.cities = self.table['city']
 		# Добавляем город в серию(pandas.Series) использованных городов
 		self.used_cities.at[len(self.used_cities)] = word
+
+	# Обрезаем название норода до тех пор, пока в таблице с
+	# городами не будет городов на его последнюю букву
+	def prepare_city(self, word):
+		while len(self.table[self.cities.str.startswith(word[-1])]['city']) == 0:
+			if word[-1] not in self.lost_letters:
+				self.lost_letters.append(word[-1])
+				self.letters_to_notify.append(word[-1])
+			word = word[:-1]
+			if word == '':
+				return word
+		return word
 
 	def reboot(self):
 		self.table = pd.read_csv('cities.csv',
